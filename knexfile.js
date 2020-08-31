8ec59a04e5db6bcf1cb4fde2e4c0008a43373352
miku0129 <svartkatt39@gmail.com>
2020-08-31 21:15:15 +0900

    succeed run migration, create table in database

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://postgres:Miku3039@localhost:5432/peoples",
+    connection:
+      "postgres://postgres:Miku3039@localhost:5432/cc14_api_solo_project",
     migrations: {
       directory: __dirname + "/db/migrations",
     },
     seeds: {
       directory: __dirname + "/db/seeds",
     },
+    charset: "utf8",
   },
 
   production: {
